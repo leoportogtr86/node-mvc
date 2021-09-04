4755ce0a9c55d60e43f618b43e2c3bb24b1b6273
Leo Porto <leonardoreisporto@gmail.com>
2021-09-04 19:31:31 -0300

    subs var por const

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var bodyParser = require('body-parser')
-var express = require("express")
-var app = express()
-var router = require("./routes/routes")
+//Curso de node => https://www.udemy.com/course/formacao-nodejs/learn/lecture/21390006#questions
+
+const bodyParser = require('body-parser')
+const express = require("express")
+const app = express()
+const router = require("./routes/routes")
  
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
